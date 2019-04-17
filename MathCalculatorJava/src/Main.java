90de76b2fd9bedf3b7ef4a8bb773e66e188e0289
MrAsieru <asiescp@gmail.com>
2019-04-17 18:26:42 +0200

    a

--- MathCalculatorJava/src/Main.java
+++ MathCalculatorJava/src/Main.java
@@ -9,17 +9,16 @@ public class Main {
 			if(args[1].equals("-h")) {
 				Ayuda(2);
 			} else if(args[1].equals("-d")) {
-				Integer[][] M = new Integer[Integer.parseInt(args[2])] [Integer.parseInt(args[3])];
+				Double[][] M = new Double[Integer.parseInt(args[2])] [Integer.parseInt(args[3])];
 				
-				ArrayList<Integer> ints = new ArrayList<>();
 				Integer c = 4;
 				for(int i = 0; i < Integer.parseInt(args[2]); i++) {
 					for(int j = 0; j < Integer.parseInt(args[3]); j++) {
-						M[i][j] = Integer.parseInt(args[c]);
+						M[i][j] = Double.parseDouble(args[c]);
 						c++;
 					}
 				}
-				Double result = Matrices.Determinante(M, Integer.parseInt(args[2]), Integer.parseInt(args[3]));
+				Double result = Matrices.Determinante(M);
 				if(Integer.parseInt(args[2]) == Integer.parseInt(args[2]) && Integer.parseInt(args[2]) == 2) {
 					System.out.println("\t    |\t"+M[0][0]+"\t"+M[0][1]+"\t|");
 					System.out.println("\t|M|=|\t"+M[1][0]+"\t"+M[1][1]+"\t|= "+result);
@@ -34,26 +33,43 @@ public class Main {
 					System.out.println("\t    |\t"+M[3][0]+"\t"+M[3][1]+"\t"+M[3][2]+"\t"+M[3][3]+"\t|");
 				}
 			} else if (args[1].equals("-i")) {
+				Double[][] M = new Double[Integer.parseInt(args[2])] [Integer.parseInt(args[3])];
 				
+				Integer c = 4;
+				for(int i = 0; i < Integer.parseInt(args[2]); i++) {
+					for(int j = 0; j < Integer.parseInt(args[3]); j++) {
+						M[i][j] = Double.parseDouble(args[c]);
+						c++;
+					}
+				}
+				Double D = Matrices.Determinante(M);
+				Double[][] R = Matrices.Invertida(M);
+				System.out.println("\t    |\t"+M[0][0]+"\t"+M[0][1]+"\t"+M[0][2]+"\t|");
+				System.out.println("\t M =|\t"+M[1][0]+"\t"+M[1][1]+"\t"+M[1][2]+"\t|");
+				System.out.println("\t    |\t"+M[2][0]+"\t"+M[2][1]+"\t"+M[2][2]+"\t|");				
+				System.out.println("\t      |\t"+R[0][0]*D+"/"+D+"\t"+R[0][1]*D+"/"+D+"\t"+R[0][2]*D+"/"+D+"\t|   |\t"+R[0][0]+"\t"+R[0][1]+"\t"+R[0][2]+"\t|");
+				System.out.println("\t M⁻¹ =|\t"+R[1][0]*D+"/"+D+"\t"+R[1][1]*D+"/"+D+"\t"+R[1][2]*D+"/"+D+"\t| = |\t"+R[1][0]+"\t"+R[1][1]+"\t"+R[1][2]+"\t|");
+				System.out.println("\t      |\t"+R[2][0]*D+"/"+D+"\t"+R[2][1]*D+"/"+D+"\t"+R[2][2]*D+"/"+D+"\t|   |\t"+R[2][0]+"\t"+R[2][1]+"\t"+R[2][2]+"\t|");
 			} else {
 				System.out.println("-d : Determinante max 4x4 Ex. -m -d <filas> <columnas> (a11) (a12) (a13) ... (afc)");
 			}
 		}
-		
-		static void Ayuda (Integer i){
-			switch(i) {
-				case 1:
-					System.out.println("Math Calculator help:");
-					System.out.println("-m : Matrices");
-					break;
-				case 2:
-					System.out.println("-d : Determinante max 4x4 Ex. -m -d <filas> <columnas> (a11) (a12) (a13) ... (afc) \n"+
-						   "-i : Matriz invertida 3x3 Ex -m -i (a11) (a12) (a13) ... (afc)");
-					break;
-				case 3:
-					break;
-					
-			}
+	}
+	
+	
+	static void Ayuda (Integer i){
+		switch(i) {
+			case 1:
+				System.out.println("Math Calculator help:");
+				System.out.println("-m : Matrices");
+				break;
+			case 2:
+				System.out.println("-d : Determinante max 4x4 Ex. -m -d <filas> <columnas> (a11) (a12) (a13) ... (afc) \n"+
+					   "-i : Matriz invertida 3x3 Ex -m -i (a11) (a12) (a13) ... (afc)");
+				break;
+			case 3:
+				break;
+				
 		}
 	}
 }
--- MathCalculatorJava/src/Matrices.java
+++ MathCalculatorJava/src/Matrices.java
@@ -1,82 +1,270 @@
+import java.util.Arrays;
+
 public class Matrices {
-  public static void Matrices(){ 
+  public Matrices(){ 
   }
   
-  static Double Determinante(Integer[][] M, Integer f, Integer c) {
+  static Double Determinante(Double[][] M) {
+	  System.out.println("Determinante: "+Arrays.deepToString(M));
 		Double result = 0.0;
-		if(f== c && f == 2) {
-			result = (double) ((M[0][0]*M[1][1]) - (M[0][1]*M[1][0]));
-		} else if(f == c && f == 3) {
-			result = (double) (((M[0][0]*M[1][1]*M[2][2])+(M[0][1]*M[1][2]*M[2][0])+(M[1][0]*M[2][1]*M[0][2])) - ((M[0][2]*M[1][1]*M[2][0])+(M[0][1]*M[1][0]*M[2][2])+(M[1][2]*M[2][1]*M[0][0])));
-		} else if (f == c && f == 4) {
-			Double tot = 0.0;
-			for(int i = 1; i < 5; i++) {
-				Integer[][] A = new Integer[3][3];
-				if(i == 1) {
-					Integer[][] C = {
-							{M[1][1], M[1][2], M[1][3]}, 
-							{M[2][1], M[2][2], M[2][3]},
-							{M[3][1], M[3][2], M[3][3]}
-					};
-					A = C;
-				} else if (i == 2) {
-					Integer[][] C = {
-							{M[0][1], M[0][2], M[0][3]}, 
-							{M[2][1], M[2][2], M[2][3]},
-							{M[3][1], M[3][2], M[3][3]}
-					};
-					A = C;
-				} else if (i == 3) {
-					Integer[][] C = {
-							{M[0][1], M[0][2], M[0][3]}, 
-							{M[1][1], M[1][2], M[1][3]},
-							{M[3][1], M[3][2], M[3][3]}
-					};
-					A = C;
-				} else if (i == 4) {
-					Integer[][] C = {
-							{M[0][1], M[0][2], M[0][3]}, 
-							{M[1][1], M[1][2], M[1][3]},
-							{M[2][1], M[2][2], M[2][3]}
-					};					
-					A = C;
-				}
-				
-				tot = tot + Math.pow(-1, i+1)*M[i-1][0]*Determinante(A, 3, 3);
-			}
-			result = tot;
+		if(M.length == M[0].length && M[0].length == 2) {
+			result = M[0][0]*M[1][1]-M[0][1]*M[1][0];
+		} else if(M.length == M[0].length && M[0].length == 3) {
+			result = M[0][0]*Determinante(Minor(M, 1))-M[1][0]*Determinante(Minor(M, 4))+M[2][0]*Determinante(Minor(M, 7));
+		} else if (M.length == M[0].length && M[0].length == 4) {			
+			result = M[0][0]*Determinante(Minor(M, 1))-M[1][0]*Determinante(Minor(M, 5))+M[2][0]*Determinante(Minor(M, 9))-M[3][0]*Determinante(Minor(M, 13));
 		}
 		return result;
 	}
 	
-	static Integer[][] Invertida(Integer[][] M) {
-		if(M[0].length == M[1].length && M[1].length == 3) {
-			if(Determinante(M, 3, 3) != 0) {
-				return M/Determinante(M, 3, 3);
+	static Double[][] Invertida(Double[][] M) {
+		System.out.println("Invertida: "+Arrays.deepToString(M));
+		if(M.length == M[0].length && M[0].length == 3) {
+			if(Determinante(M) != 0) {
+				Double[][] B = new Double[3][3];
+				Double[][] A = Adjunta(Transpuesta(M));
+				Double Deter = Determinante(M);
+				for(int i = 0; i < 3; i++) {
+					for(int j = 0; j < 3; j++) {
+						B[i][j] = A[i][j]/Deter; 
+					}
+				}
+				return B;
 			} else {
 				return null;	
 			}
-		}		
-	}
-	
-	static Integer[][] Adjunta(Integer[][] M) {
-		Integer[][] A = Transpuesta(M);
-		if(M[0].length == M[1].length && M[1].length == 3) {
-			return Integer[][] B = {
-				{Determinante(Integer[][] {{M[1][1], M[1][2]}, {M[2][1], M[2][2]}}, 2, 2), -Determinante(Integer[][] {{M[0][1], M[0][2]}, {M[2][1], M[2][2]}}, 2, 2), Determinante(Integer[][] {{M[0][1], M[0][2]}, {M[1][1], M[1][2]}}, 2, 2)},
-				{{-Determinante(Integer[][] {{M[0][1], M[0][2]}, {M[2][1], M[2][2]}}, 2, 2), Determinante(Integer[][] {{M[0][0], M[0][2]}, {M[2][0], M[2][2]}}, 2, 2), -Determinante(Integer[][] {{M[0][0], M[0][1]}, {M[2][0], M[2][1]}}, 2, 2)}},
-				{Determinante(Integer[][] {{M[0][1], M[0][2]}, {M[1][1], M[1][2]}}, 2, 2), -Determinante(Integer[][] {{M[0][0], M[0][2]}, {M[1][0], M[1][2]}}, 2, 2), Determinante(Integer[][] {{M[0][0], M[0][1]}, {M[1][0], M[1][1]}}, 2, 2)}
-			};
 		}
+		return null;		
 	}
 	
-	static Integer[][] Transpuesta(Integer[][] M) {
-		if(M[0].length == M[1].length && M[1].length == 3){
-			return Integer[][] A = {
-			{M[0][0], M[1][0]}, M[2][0],
+	static Double[][] Adjunta(Double[][] M) {
+		System.out.println("Adjunta: "+Arrays.deepToString(M));
+		if(M.length == M[0].length && M[0].length == 3) {
+			Double[] B = new Double[9];
+			for(int i = 0; i < 9; i++) {
+				B[i] = Determinante(Minor(M, i+1));
+			}
+			return new Double[][] {
+				{B[0], -B[1], B[2]},
+				{-B[3], B[4], -B[5]},
+				{B[6], -B[7], B[8]}
+			};			
+		}
+		return null;
+	}
+	
+	static Double[][] Transpuesta(Double[][] M) {
+		System.out.println("Transpuesta: "+Arrays.deepToString(M));
+		if(M.length == M[0].length && M[0].length == 3){
+			return new Double[][] {
+			{M[0][0], M[1][0], M[2][0]},
 			{M[0][1], M[1][1], M[2][1]},
 			{M[0][2], M[1][2], M[2][2]}
 		};
-		}		
+		}
+		return null;
+	}
+	
+	static Double[][] Minor(Double[][] M, Integer intMinor){
+		System.out.println("Minor: "+Arrays.deepToString(M));
+		if(M.length == M[0].length && M[0].length == 2) {
+			switch(intMinor) {
+			case 1:
+				Double[][] A1 = {
+						{M[1][1]},						
+					};
+				return A1;
+			case 2:
+				Double[][] A2 = {
+						{M[1][0]},						
+					};
+				return A2;
+			case 3:
+				Double[][] A3 = {
+						{M[0][1]},						
+					};
+				return A3;
+			case 4:
+				Double[][] A4 = {
+						{M[0][0]},						
+					};
+				return A4;
+			}			
+		} else if (M.length == M[0].length && M[0].length == 3) {
+			switch (intMinor) {
+			case 1:
+				Double[][] A1 = {
+						{M[1][1], M[1][2]},
+						{M[2][1], M[2][2]}						
+					};
+				return A1;				
+			case 2:
+				Double[][] A2 = {
+						{M[1][0], M[1][2]},
+						{M[2][0], M[2][2]}						
+					};
+				return A2;
+			case 3:
+				Double[][] A3 = {
+						{M[1][0], M[1][1]},
+						{M[2][0], M[2][1]}						
+					};
+				return A3;
+			case 4:
+				Double[][] A4 = {
+						{M[0][1], M[0][2]},
+						{M[2][1], M[2][2]}						
+					};
+				return A4;
+			case 5:
+				Double[][] A5 = {
+						{M[0][0], M[0][2]},
+						{M[2][0], M[2][2]}						
+					};
+				return A5;
+			case 6:
+				Double[][] A6 = {
+						{M[0][0], M[0][1]},
+						{M[2][0], M[2][1]}						
+					};
+				return A6;
+			case 7:
+				Double[][] A7 = {
+						{M[0][1], M[0][2]},
+						{M[1][1], M[1][2]}						
+					};
+				return A7;
+			case 8:
+				Double[][] A8 = {
+						{M[0][0], M[0][2]},
+						{M[1][0], M[1][2]}						
+					};
+				return A8;
+			case 9:
+				Double[][] A9 = {
+						{M[0][0], M[0][1]},
+						{M[1][0], M[1][1]}						
+					};
+				return A9;
+			}
+		} else if(M.length == M[0].length && M[0].length == 4) {
+			switch (intMinor) {
+			case 1:
+				Double[][] A1 = {
+						{M[1][1], M[1][2], M[1][3]},
+						{M[2][1], M[2][2], M[2][3]},
+						{M[3][1], M[3][2], M[3][3]}
+					};
+				return A1;
+			case 2:
+				Double[][] A2 = {
+						{M[1][0], M[1][2], M[1][3]},
+						{M[2][0], M[2][2], M[2][3]},
+						{M[3][0], M[3][2], M[3][3]}
+					};
+				return A2;
+			case 3:
+				Double[][] A3 = {
+						{M[1][0], M[1][1], M[1][3]},
+						{M[2][0], M[2][1], M[2][3]},
+						{M[3][0], M[3][1], M[3][3]}
+					};
+				return A3;
+			case 4:
+				Double[][] A4 = {
+						{M[1][0], M[1][1], M[1][2]},
+						{M[2][0], M[2][1], M[2][2]},
+						{M[3][0], M[3][1], M[3][2]}
+					};
+				return A4;
+			case 5:
+				Double[][] A5 = {
+						{M[0][1], M[0][2], M[0][3]},
+						{M[2][1], M[2][2], M[2][3]},
+						{M[3][1], M[3][2], M[3][3]}
+					};
+				return A5;
+			case 6:
+				Double[][] A6 = {
+						{M[0][0], M[0][2], M[0][3]},
+						{M[2][0], M[2][2], M[2][3]},
+						{M[3][0], M[3][2], M[3][3]}
+					};
+				return A6;
+			case 7:
+				Double[][] A7 = {
+						{M[0][0], M[0][1], M[0][3]},
+						{M[2][0], M[2][1], M[2][3]},
+						{M[3][0], M[3][1], M[3][3]}
+					};
+				return A7;
+			case 8:
+				Double[][] A8 = {
+						{M[0][0], M[0][1], M[0][2]},
+						{M[2][0], M[2][1], M[2][2]},
+						{M[3][0], M[3][1], M[3][2]}
+					};
+				return A8;
+			case 9:
+				Double[][] A9 = {
+						{M[0][1], M[0][2], M[0][3]},
+						{M[1][1], M[1][2], M[1][3]},
+						{M[3][1], M[3][2], M[3][3]}
+					};
+				return A9;
+			case 10:
+				Double[][] A10 = {
+						{M[0][0], M[0][2], M[0][3]},
+						{M[1][0], M[1][2], M[1][3]},
+						{M[3][0], M[3][2], M[3][3]}
+					};
+				return A10;
+			case 11:
+				Double[][] A11 = {
+						{M[0][0], M[0][1], M[0][3]},
+						{M[1][0], M[1][1], M[1][3]},
+						{M[3][0], M[3][1], M[3][3]}
+					};
+				return A11;
+			case 12:
+				Double[][] A12 = {
+						{M[0][0], M[0][1], M[0][2]},
+						{M[1][0], M[1][1], M[1][2]},
+						{M[3][0], M[3][1], M[3][2]}
+					};
+				return A12;
+			case 13:
+				Double[][] A13 = {
+						{M[0][1], M[0][2], M[0][3]},
+						{M[1][1], M[1][2], M[1][3]},
+						{M[2][1], M[2][2], M[2][3]}
+					};
+				return A13;
+			case 14:
+				Double[][] A14 = {
+						{M[0][0], M[0][2], M[0][3]},
+						{M[1][0], M[1][2], M[1][3]},
+						{M[2][0], M[2][2], M[2][3]}
+					};
+				return A14;
+			case 15:
+				Double[][] A15 = {
+						{M[0][0], M[0][1], M[0][3]},
+						{M[1][0], M[1][1], M[1][3]},
+						{M[2][0], M[2][1], M[2][3]}
+					};
+				return A15;
+			case 16:
+				Double[][] A16 = {
+						{M[0][0], M[0][1], M[0][2]},
+						{M[1][0], M[1][1], M[1][2]},
+						{M[2][0], M[2][1], M[2][2]}
+					};
+				return A16;
+			}
+			
+			
+		}
+		return null;
 	}
 }
